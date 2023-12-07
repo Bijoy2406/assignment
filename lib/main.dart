d7d7a30ce025af54f72fa9ad8a1244c6c40db82b
Your Name <you@example.com>
2023-12-07 13:01:51 +0600

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductDetailsPage(),
+    );
+  }
+}
+
+class ProductDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'DM-10',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Image.network(
+              'https://static-01.daraz.com.bd/p/81df8d6bee3c11e0b15bcf08bcde8fda.jpg',
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 10),
+            Text('Product Price: ৳550 BDT'),
+            SizedBox(height: 10),
+            Text('Best low price headphone for gamers'),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BuyNowPage()),
+                );
+              },
+              child: Text('Buy Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BuyNowPage extends StatefulWidget {
+  @override
+  _BuyNowPageState createState() => _BuyNowPageState();
+}
+
+class _BuyNowPageState extends State<BuyNowPage> {
+  String? selectedPaymentMethod;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buy Now'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Choose Payment Method',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            DropdownButton<String>(
+              hint: Text('Select Payment Method'),
+              value: selectedPaymentMethod,
+              items: ['BKash', 'Nagad', 'Rocket'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? value) {
+                // Handle dropdown selection
+                setState(() {
+                  selectedPaymentMethod = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Text('Selected Payment Method: ${selectedPaymentMethod ?? "None"}'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentMethodButton extends StatelessWidget {
+  final String label;
+  final ValueChanged<String?>? onChanged;
+  final List<String> dropdownItems;
+
+  const PaymentMethodButton({
+    required this.label,
+    required this.onChanged,
+    required this.dropdownItems,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(label),
+        SizedBox(height: 10),
+        DropdownButton<String>(
+          items: dropdownItems.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
